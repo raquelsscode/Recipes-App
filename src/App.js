8f703503568178889a5874a4c9ad340b490f366b
Gisele <gisele.viedenhelfen@gmail.com>
2022-06-21 18:48:10 -0300

    adiciona barra de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import TelaDeReceitas from './Components/TelaDeReceitas';
+import Login from './Components/Login';
 // xablau enorme rs
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+    <div>
+      <Switch>
+        <Route exact path="/foods" component={ TelaDeReceitas } />
+        <Route exact path="/" component={ Login } />
+      </Switch>
     </div>
   );
 }
--- src/Components/searchHeader.js
+++ src/Components/searchHeader.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
+import { requestIngredient, requestName,
+  requestFirstLetter } from '../services/RequestAPI';
 
 class HeaderSearch extends Component {
+  constructor() {
+    super();
+    this.state = {
+      radioSelected: '',
+      inputTextValue: '',
+    };
+    this.handleClickRadio = this.handleClickRadio.bind(this);
+    this.handleClickSubmit = this.handleClickSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleClickRadio({ target }) {
+    this.setState({ radioSelected: target.value });
+  }
+
+  handleChange({ target }) {
+    this.setState({ inputTextValue: target.value });
+  }
+
+  handleClickSubmit() {
+    const { radioSelected, inputTextValue } = this.state;
+    if (radioSelected === 'ingredient') {
+      requestIngredient(inputTextValue);
+    }
+    if (radioSelected === 'name') {
+      requestName(inputTextValue);
+    }
+    if (radioSelected === 'first-letter') {
+      if (inputTextValue.length > 1) {
+        global.alert('Your search must have only 1 (one) character');
+      }
+      requestFirstLetter(inputTextValue);
+    }
+  }
+
   render() {
     return (
       <form>
@@ -9,6 +46,9 @@ class HeaderSearch extends Component {
             type="radio"
             id="ingredient-search-radio"
             data-testid="ingredient-search-radio"
+            name="search-type"
+            value="ingredient"
+            onClick={ this.handleClickRadio }
           />
           Busca por ingrediente
         </label>
@@ -16,7 +56,10 @@ class HeaderSearch extends Component {
           <input
             type="radio"
             id="name-search-radio"
+            name="search-type"
             data-testeid="name-search-radio"
+            value="name"
+            onClick={ this.handleClickRadio }
           />
           Busca por nome
         </label>
@@ -25,12 +68,20 @@ class HeaderSearch extends Component {
             type="radio"
             id="first-letter-search-radio"
             data-testeid="first-letter-search-radio"
+            name="search-type"
+            value="first-letter"
+            onClick={ this.handleClickRadio }
           />
           Busca da primeira letra
         </label>
+        <input
+          type="text"
+          onChange={ this.handleChange }
+        />
         <button
           type="button"
           data-testid="exec-search-btn"
+          onClick={ this.handleClickSubmit() }
         >
           Buscar
         </button>
