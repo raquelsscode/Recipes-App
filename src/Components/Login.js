bb7e4b98b14ba5a747d4c4811b0aac0dec7e7f46
Matp1 <matheus.paivaads@gmail.com>
2022-06-22 16:51:20 -0300

    profile screen

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../App.css';
-import rockGlass from '../images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import rockGlass from '../images/rockGlass.svg';
 
 class Login extends React.Component {
   constructor() {
