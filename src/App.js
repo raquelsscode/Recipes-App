76350c9b8355bd216869657a2ed0aba41c35680f
Matp1 <matheus.paivaads@gmail.com>
2022-06-22 16:33:24 -0300

    fix lint errors

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import Login from './Components/Login';
 import Profile from './pages/Profile';
 import favoriteRecipes from './pages/favoriteRecipes';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">Receitas</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route exact path="/profile" component={ Profile } />
