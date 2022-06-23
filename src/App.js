9db5077139857855ed7a027d0f379a7812039721
Matp1 <matheus.paivaads@gmail.com>
2022-06-23 16:34:51 -0300

    explore screen checked

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Login from './Components/Login';
 import Profile from './pages/Profile';
 import favoriteRecipes from './pages/favoriteRecipes';
 import TelaDeReceitas from './Components/TelaDeReceitas';
+import Explore from './Components/Explore';
+import ExploreFoods from './pages/ExploreFoods';
+import ExploreDrinks from './pages/ExploreDrinks';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/profile" component={ Profile } />
         <Route exact path="/favorite-recipes" component={ favoriteRecipes } />
+        <Route exact path="/explore" component={ Explore } />
+        <Route exact path="/explore/foods" component={ ExploreFoods } />
+        <Route exact path="/explore/drinks" component={ ExploreDrinks } />
       </Switch>
     </div>
   );
